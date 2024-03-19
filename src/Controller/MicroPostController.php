9a03079d5152ffa2d2ec4df3d0a931a58c1b3b4c
rafaPedrosa <rafa.pedrosa@atexis.com>
2024-03-19 14:09:32 +0100

    Databases and doctrine orm

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +13,37 @@ use Symfony\Component\Routing\Attribute\Route;
 class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
-    public function index(): Response
+    public function index(MicroPostRepository $posts): Response
     {
         return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
+            'posts' => $posts->findAll(),
         ]);
     }
+
+    #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    public function showOne(MicroPost $post):Response
+    {
+        return $this->render("micro_post/show.html.twig",[
+            "post" => $post,
+        ]);
+    }
+
+    // #[Route('/micro-post/create', name: 'app_micro_post_create')]
+    // public function createMicroPost(EntityManagerInterface $entityManager): Response
+    // {
+    //     $microPost = new MicroPost();
+    //     $microPost->setTitle("It comes from controller");
+    //     $microPost->setText("Hi!");
+    //     $microPost->setCreated(new DateTime());
+
+    //     $entityManager->persist($microPost);
+    //     $entityManager->flush();
+
+    //     // dd($posts->findOneBy(["title"=>"Welcome to Atexis!"]));
+    //     return $this->render('micro_post/index.html.twig', [
+    //         'response' => "Saved new micro post with id ". $microPost->getId(),
+    //     ]);
+    // }
+
+
 }
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\MicroPost;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+use function Symfony\Component\Clock\now;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $microPost1 = new MicroPost;
+        $microPost1->setTitle("Welcome to Atexis!");
+        $microPost1->setText("Welcome to Atexis again!");
+        $microPost1->setCreated(new DateTime());
+        $manager->persist($microPost1);
+
+        $microPost2 = new MicroPost;
+        $microPost2->setTitle("Welcome to Spain!");
+        $microPost2->setText("Welcome to Spain again!");
+        $microPost2->setCreated(new DateTime());
+        $manager->persist($microPost2);
+
+        $microPost3 = new MicroPost;
+        $microPost3->setTitle("Welcome to Seville!");
+        $microPost3->setText("Welcome to Seville again!");
+        $microPost3->setCreated(new DateTime());
+        $manager->persist($microPost3);
+
+        $manager->flush();
+    }
+}
