f587619e16df80de6b9eb92ab6d8950a0dc0a1e4
rafaPedrosa <rafa.pedrosa@atexis.com>
2024-03-20 13:39:38 +0100

    Database relations

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,9 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use App\Entity\User;
+use App\Entity\Comment;
+use App\Entity\MicroPost;
+use App\Entity\UserProfile;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\UserProfileRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HelloController extends AbstractController
 {
@@ -15,14 +23,52 @@ class HelloController extends AbstractController
         ['message' => 'Bye!', 'created' => '2022/05/12']
     ];
 
-    #[Route('/messages{limit<\d+>?3}', name: 'app_message')]
-    public function index(int $limit): Response
+    #[Route('/messages', name: 'app_message')]
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        // Create a new MicroPost and comment and assign the comment to it
+
+        // $post = new MicroPost();
+        // $post->setTitle('Hello');
+        // $post->setText('Hello');
+        // $post->setCreated(new DateTime());
+
+        // $post = $entityManager->getRepository(MicroPost::class)->find(11);
+        // $comment = $post->getComments()[0];
+
+        // $post->removeComment($comment);
+
+        // $entityManager->persist($post);
+        // $entityManager->flush();
+
+
+        // $comment = new Comment();
+        // $comment->setText('Hello');
+        // $comment->setPost($post);
+        // // $post->addComment($comment);
+
+        // // $entityManager->persist($post);
+
+        // Create a new user and assign it to a new profile
+
+        // $user = new User();
+        // $user->setEmail('email@email.com');
+        // $user->setPassword('12345678');
+
+        // $profile = new UserProfile();
+        // $profile->setUser($user);
+        // $entityManager->persist($profile);
+        // $entityManager->flush();
+
+        // // $profile = $entityManager->getRepository(UserProfile::class)->find(1);
+        // // $entityManager->remove($profile);
+        // // $entityManager->flush();
+
         return $this->render(
             'hello/index.html.twig',
             [
                 'messages' => $this->messages,
-                'limit' => $limit
+                'limit' => 3
             ]
         );
     }
