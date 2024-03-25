fa5ed691cd0ffbe4e231ce500b5fe7bb1d32c4ab
rafaPedrosa <rafa.pedrosa@atexis.com>
2024-03-25 13:52:05 +0100

    Image upload logic

--- src/Controller/SettingsProfileController.php
+++ src/Controller/SettingsProfileController.php
@@ -1,40 +1,39 @@
 <?php
-
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\UserProfile;
 use App\Form\UserProfileType;
 use App\Form\ProfileImageType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 class SettingsProfileController extends AbstractController
 {
     #[Route('/settings/profile', name: 'app_settings_profile')]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function profile(
         Request $request,
-        EntityManagerInterface $entityManager
-    ): Response
-    {
+        UserRepository $users
+    ): Response {
         /** @var User $user */
         $user = $this->getUser();
         $userProfile = $user->getUserProfile() ?? new UserProfile();
-
         $form = $this->createForm(
-            UserProfileType::class, $userProfile
+            UserProfileType::class,
+            $userProfile
         );
         $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $userProfile = $form->getData();
             $user->setUserProfile($userProfile);
-            $entityManager->persist($user);
-            $entityManager->flush();
+            $users->add($user, true);
             $this->addFlash(
                 'success',
                 'Your user profile settings were saved.'
@@ -43,18 +42,55 @@ class SettingsProfileController extends AbstractController
                 'app_settings_profile'
             );
         }
-
-        return $this->render('settings_profile/profile.html.twig', [
-            'form' => $form->createView(),
-        ]);
+        return $this->render(
+            'settings_profile/profile.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
     }
 
     #[Route('/settings/profile-image', name: 'app_settings_profile_image')]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function profileImage(): Response
-    {
+public function profileImage(
+        Request $request,
+        SluggerInterface $slugger,
+        EntityManagerInterface $entityManager
+    ): Response {
         $form = $this->createForm(ProfileImageType::class);
-        
+        /** @var User $user */
+        $user = $this->getUser();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $profileImageFile = $form->get('profileImage')->getData();
+
+            if ($profileImageFile) {
+                $originalFileName = pathinfo(
+                    $profileImageFile->getClientOriginalName(),
+                    PATHINFO_FILENAME
+                );
+                $safeFilename = $slugger->slug($originalFileName);
+                $newFileName = $safeFilename . '-' . uniqid() . '.' . $profileImageFile->guessExtension();
+
+                try {
+                    $profileImageFile->move(
+                        $this->getParameter('profiles_directory'),
+                        $newFileName
+                    );
+                } catch (FileException $e) {
+                }
+
+                $profile = $user->getUserProfile() ?? new UserProfile();
+                $profile->setImage($newFileName);
+                $entityManager->persist($user);
+                $entityManager->flush();
+                $this->addFlash('success', 'Your profile image was updated.');
+
+                return $this->redirectToRoute('app_settings_profile_image');
+            }
+        }
+
         return $this->render(
             'settings_profile/profile_image.html.twig',
             [
