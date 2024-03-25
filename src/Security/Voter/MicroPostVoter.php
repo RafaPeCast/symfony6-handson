651acc48edc57d2a0dffd9d0e23ab6335e227bc7
rafaPedrosa <rafa.pedrosa@atexis.com>
2024-03-25 14:52:35 +0100

    MicroPostVoter fixed

--- src/Security/Voter/MicroPostVoter.php
+++ src/Security/Voter/MicroPostVoter.php
@@ -13,10 +13,10 @@ class MicroPostVoter extends Voter
 {
     public function __construct(
         private Security $security
-    ){     
+    ) {
     }
 
-    protected function supports(string $attribute, mixed $subject): bool
+    protected function supports(string $attribute, $subject): bool
     {
         // replace with your own logic
         // https://symfony.com/doc/current/security/voters.html
@@ -27,46 +27,40 @@ class MicroPostVoter extends Voter
     /**
      * @param MicroPost $subject
      */
-
     protected function voteOnAttribute(
         string $attribute,
-        mixed $subject, 
-        TokenInterface $token): bool
-    {
-        /** @var User $user */
+        $subject,
+        TokenInterface $token
+    ): bool {
+        /** @var User $user  */
         $user = $token->getUser();
-
         // if the user is anonymous, do not grant access
         // if (!$user instanceof UserInterface) {
         //     return false;
         // }
-
         $isAuth = $user instanceof UserInterface;
 
-        if ($this->security->isGranted('ROLE_ADMIN')){
+        if ($this->security->isGranted('ROLE_ADMIN')) {
             return true;
         }
 
         // ... (check conditions and return true to grant permission) ...
         switch ($attribute) {
             case MicroPost::EDIT:
-                return $isAuth 
-                && (
-                    (
-                    $this->security->isGranted('ROLE_EDITOR')
-            );
-
+                return $isAuth
+                    && (
+                        ($subject->getAuthor()->getId() === $user->getId()) ||
+                        $this->security->isGranted('ROLE_EDITOR')
+                    );
             case MicroPost::VIEW:
-                if(!$subject->isExtraPrivacy()){
+                if (!$subject->isExtraPrivacy()) {
                     return true;
                 }
 
-                return $isAuth && 
-                    ($subject->getAuthor()->getId() === $user->getId()) 
+                return $isAuth &&
+                    ($subject->getAuthor()->getId() === $user->getId()
                         || $subject->getAuthor()->getFollows()->contains($user)
-
-            );
-                    
+                    );
         }
 
         return false;
