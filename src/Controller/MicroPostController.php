3ed60930dffad4a6dd51be393f1b75a4fbdc7581
rafaPedrosa <rafa.pedrosa@atexis.com>
2024-03-26 10:10:07 +0100

    Darkmode is no longer default and the landing page has been changed

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -18,7 +18,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MicroPostController extends AbstractController
 {
-    #[Route('/micro-post', name: 'app_micro_post')]
+    #[Route('/', name: 'app_micro_post')]
     public function index(MicroPostRepository $posts): Response
     {
         return $this->render('micro_post/index.html.twig', [
